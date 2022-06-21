89a9c83ec9eb5ec789e33a3f2b66262b5394f6c5
wyfyjc <wyfyjc@foxmail.com>
2022-06-22 01:22:35 +0800

    貌似可以使用了

--- Index.js
+++ Index.js
@@ -1,4 +1,13 @@
 window.onload = function () {
+    localStorage.removeItem(null);
+    
+    <!-- 初始化默认搜索引擎 -->
+    let search_engine = document.getElementById('search_engine');
+    for (let i = 0; i < search_engine.length; i++) {
+        if (search_engine.options[i].text === localStorage.getItem('search_engine')) {
+            search_engine.options[i].setAttribute('selected', true);
+        }
+    }
 };
 
 <!-- 发起搜索（Bing） -->
@@ -21,7 +30,7 @@ function search_input() {
                 localStorage.setItem('search_engine', 'Bing');
                 break;
             case 'Baidu':
-                search_url = search_url.concat('https://baidu.com/search?wd=');
+                search_url = search_url.concat('https://baidu.com/s?wd=');
                 localStorage.setItem('search_engine', 'Baidu');
                 break;
         }
@@ -32,34 +41,47 @@ function search_input() {
         
         <!-- 追加已屏蔽网站 -->
         for (let i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i) === 'search_engine') {
+                continue;
+            }
             let site = localStorage.getItem(String(i));
             site = '+-site%3A'.concat(site);
             search_url = search_url.concat(site);
         }
         
+        console.log(search_url);
         window.open(search_url);
     }
 }
 
 <!-- 新增屏蔽 -->
 function insert_site() {
-    let site = document.getElementById('insert_site').value;
-    localStorage.setItem(String(localStorage.length), site);
-    document.getElementById('insert_site').value = null;
+    <!-- 写入屏蔽网站，并清空内容 -->
+    let site = document.getElementById('insert_site');
+    if (site.value === '') {
+        return;
+    }
+    localStorage.setItem(String(localStorage.length), site.value);
+    site.value = '';
+    
+    <!-- 刷新并展开屏蔽列表 -->
+    blocked_sites();
+    document.getElementById('blocked_sites_details').setAttribute('open', '');
 }
 
 <!-- 已屏蔽网站 -->
 function blocked_sites() {
     let blocked_sites = document.getElementById('blocked_sites');
-    blocked_sites.innerHTML = '';
+    blocked_sites.innerText = '';
+    
+    let item = '';
     for (let i = 0; i < localStorage.length; i++) {
-        let p = document.createElement('p');
-        p.innerText = localStorage.getItem(String(i));
+        if (localStorage.key(i) === 'search_engine') {
+            continue;
+        }
         
-        let span = document.createElement('span');
-        // span.innerText = '×';
-        
-        blocked_sites.appendChild(p);
-        blocked_sites.appendChild(span);
+        item = item.concat(localStorage.getItem(String(i)) + '\n');
     }
+    
+    blocked_sites.innerHTML = item;
 }
